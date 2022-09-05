d781e57d69f263f902ae9ddf0e6a58595f66b71d
Andres Agudelo <anagudelogu@gmail.com>
2022-09-05 16:20:55 -0500

    Implement desired behavior of registration feature

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe 'Validations' do
+    it 'should have a Name' do
+      user.name = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should have an email' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should have a password' do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
