2ae400adcfae014edf8bd284ee6150f16a022c79
Andres Agudelo <anagudelogu@gmail.com>
2022-09-05 14:30:41 -0500

    Create Devise User and remove turbo from devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'log_in', sign_up: 'register'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "splash_page#index"
