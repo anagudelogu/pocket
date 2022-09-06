87b7345cc94fc143574081ce8c7c31fe16d9e9df
Andres Agudelo <anagudelogu@gmail.com>
2022-09-06 11:13:25 -0500

    Implement Categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @categories = Category.where(user: current_user)
+  end
+
+  def new
+    @category = current_user.categories.build
+  end
 end
