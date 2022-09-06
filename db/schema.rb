1e264c24bb9b9f8e4137ea1c09a3504e52c81f6e
Andres Agudelo <anagudelogu@gmail.com>
2022-09-06 15:53:27 -0500

    Create categories_expenses join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_195335) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_204911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_195335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_expenses", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
   end
 
   create_table "expenses", force: :cascade do |t|
