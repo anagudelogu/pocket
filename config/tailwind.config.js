991fc26f11c20c3d25aba7fd495f9a093a4d1d49
Andres Agudelo <anagudelogu@gmail.com>
2022-09-07 18:38:43 -0500

    Install Proxima Nova font (Light/bold) and set design colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        proxima_bold: ['Proxima Nova Bold', 'sans-serif'],
+        proxima_light: ['Proxima Nova Light', 'sans-serif'],
+      },
+      colors: {
+        blue: '#3778c2',
+        green: '#5fb523',
+        gray: '#434b54',
       },
     },
   },
@@ -18,5 +25,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};
